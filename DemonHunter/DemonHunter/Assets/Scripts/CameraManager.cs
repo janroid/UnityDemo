e7b2cb323ae56d23887b0aec55bfab9108da579e
janroid <JanRoid@boyaa.com>
2018-09-12 14:32:07 +0800

    feat：实现人物移动，相机跟随，转向

---
+++ DemonHunter/DemonHunter/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour {
+
+    private Transform player;
+    private Transform mycamera;
+
+    public float smooth = 3f;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag(Tags.Player).transform;
+        mycamera = GetComponent<Transform>();
+        mycamera.LookAt(player);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector3 newPos = player.position + new Vector3(0,6,-6);
+
+        mycamera.position = Vector3.Lerp(mycamera.position,newPos,Time.deltaTime * smooth);
+	}
+}
---
+++ DemonHunter/DemonHunter/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    public int speed = 5;
+
+    private Rigidbody myrigidbody;
+    private Transform player;
+    private Animator anim;
+    private int groundIndex;
+    private Camera mainCamera;
+
+    public const float PRECISION = 0.000001f;
+
+	// Use this for initialization
+	void Start () {
+        myrigidbody = GetComponent<Rigidbody>();
+        player = GetComponent<Transform>();
+        anim = GetComponent<Animator>();
+        groundIndex = LayerMask.GetMask("Ground");
+        mainCamera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        // 载具类型的移动
+        //Vector3 globalDirectionForward = player.TransformDirection(Vector3.forward);
+        //Vector3 ForwardDirection = z * globalDirectionForward;//物体前后移动的方向
+        //Vector3 GlobalDirectionRight = player.TransformDirection(Vector3.right);
+        //Vector3 RightDirection = x * GlobalDirectionRight;//物体左右移动的方向
+        //Vector3 MainDirection = ForwardDirection + RightDirection;
+        //方案一
+        //myrigidbody.AddForce(MainDirection * speed, ForceMode.Force);
+        //方案二
+        //myrigidbody.AddRelativeForce((x * Vector3.right + z * Vector3.forward) * speed, ForceMode.Force);
+        //方案三
+        myrigidbody.velocity = new Vector3(
+            speed * x,
+            myrigidbody.velocity.y,
+            speed * z
+        );
+        //方案四
+        //角色的移动
+        //myrigidbody.MovePosition(player.position + new Vector3(x, 0, z) * speed * Time.deltaTime);
+
+        // 转向
+        Ray mouceRay = mainCamera.ScreenPointToRay(Input.mousePosition); // 鼠标发送的射线
+        RaycastHit raycastHit;
+        if (Physics.Raycast(mouceRay, out raycastHit, 200, groundIndex))
+        {
+            Vector3 target = raycastHit.point;
+            target.y = transform.position.y;
+            player.LookAt(target);
+
+        }
+
+        if (Mathf.Abs(x) > PRECISION || Mathf.Abs(z) > PRECISION)
+        {
+            anim.SetBool("Move", true);
+            anim.Play("Move");
+        }
+        else
+        {
+            anim.SetBool("Move", false);
+            anim.Play("Idle");
+        }
+
+	}
+
+}
